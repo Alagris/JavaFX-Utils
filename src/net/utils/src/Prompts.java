43c8edd376fa18df4c5e1232c9469cc20bf74827
alagris <alagris@12358>
2016-10-31 13:20:14 +0100

    modified:   bin/net/utils/src/Prompts.class 	modified:   src/net/utils/src/Prompts.java

--- src/net/utils/src/Prompts.java
+++ src/net/utils/src/Prompts.java
@@ -2,9 +2,11 @@ package net.utils.src;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Optional;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceDialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextInputDialog;
@@ -12,9 +14,11 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 
-public class Prompts {
+public class Prompts
+{
 
-	public static boolean showPrompt(String text, String header) {
+	public static boolean showPrompt(String text, String header)
+	{
 		Alert alert = new Alert(AlertType.CONFIRMATION);
 		alert.setTitle("Confirm");
 		alert.setHeaderText(header);
@@ -23,7 +27,8 @@ public class Prompts {
 		return alert.showAndWait().get() == ButtonType.OK;
 	}
 
-	public static void showErrorPrompt(String text, String header) {
+	public static void showErrorPrompt(String text, String header)
+	{
 		Alert alert = new Alert(AlertType.ERROR);
 		alert.setTitle("Error");
 		alert.setHeaderText(header);
@@ -31,7 +36,8 @@ public class Prompts {
 		alert.showAndWait();
 	}
 
-	public static void showExceptionPrompt(String text, String header, Exception ex) {
+	public static void showExceptionPrompt(String text, String header, Exception ex)
+	{
 		Alert alert = new Alert(AlertType.ERROR);
 		alert.setTitle("Exception");
 		alert.setHeaderText(header);
@@ -65,7 +71,8 @@ public class Prompts {
 		alert.showAndWait();
 	}
 
-	public static String showStringInputPrompt(String text, String header,String defaultValue) {
+	public static String showStringInputPrompt(String text, String header, String defaultValue)
+	{
 		TextInputDialog dialog = new TextInputDialog(defaultValue);
 		dialog.setTitle("Input Dialog");
 		dialog.setHeaderText(header);
@@ -75,4 +82,73 @@ public class Prompts {
 		return dialog.showAndWait().get();
 
 	}
+
+	public static Boolean showBooleanInputPrompt(String text, String header)
+	{
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Decision Dialog");
+		alert.setHeaderText(header);
+		alert.setContentText(text);
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.get() == ButtonType.OK)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/** Buttons names will be resolved by calling toString() o each object */
+	@SafeVarargs
+	public static <T> int showArrayInputPrompt(String text, String header, T... buttenNames)
+	{
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Choice Dialog");
+		alert.setHeaderText(header);
+		alert.setContentText(text);
+		ButtonType[] buttons = new ButtonType[buttenNames.length];
+		for (int i = 0; i < buttenNames.length; i++)
+		{
+			buttons[i] = new ButtonType(buttenNames[i].toString());
+		}
+		alert.getButtonTypes().setAll(buttons);
+		ButtonType result = alert.showAndWait().get();
+
+		for (int i = 0; i < buttenNames.length; i++)
+		{
+			if (result == buttons[i]) { return i; }
+		}
+		return -1;
+	}
+
+	/**
+	 * choices cannot contain null argument
+	 * 
+	 * @throws IllegalArgumentException
+	 */
+	@SafeVarargs
+	public static <T> T showComboBoxInputPrompt(String text, String header, int indexOfDefaultChoice, T... choices)
+	{
+		for (T t : choices)
+		{
+			if (t == null)
+				throw new IllegalArgumentException("Choices array contains one or more null elements");
+		}
+		ChoiceDialog<T> dialog = new ChoiceDialog<T>(choices[indexOfDefaultChoice], choices);
+		dialog.setTitle("Choice Dialog");
+		dialog.setHeaderText(header);
+		dialog.setContentText(text);
+		Optional<T> result = dialog.showAndWait();
+		if (result.isPresent())
+		{
+			return result.get();
+		}
+		else
+		{
+			return null;
+		}
+	}
+
 }
